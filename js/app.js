9869c19559132e201421ba52e8f44abb0ccd9494
pytherik <erikberndt@gmx.net>
2022-11-10 19:09:00 +0100

    add comments

--- js/app.js
+++ js/app.js
@@ -3,12 +3,22 @@ const system = {
   1: "0x",
   2: "b"
 };
-
+const richtig = ["Schönchen!", "Korrekt!", "Prima!", "Schick!", "Mega!", "Famos!", "Knorke!", "Toll!", "Hübsch!", "Fein!", "Oké!", "Jope!"];
+const falsch = ["Nope!", "Nee!", "Autsch!", "Mies!", "Falsch!", "Quatsch!", "Nö!", "Wohl kaum!", "Hmmmm...", "Stinkt!", "Kalt!", "Oh jeh!"];
 const wrong = ["😩","😞","😵","🤮","😭","😖","💩","😱","😡","🥴","😟","😕","😬","❌"];
 const right = ["😜","🤪","👻","😇","😙","😊","😃","🤓","🍬","🍺","🍰","😎","🙃","👍"];
 const d = Math.floor(Math.random() * 256);
 const all = [d, get_hex(d), get_bin(d)];
 const aufgabe = Math.floor(Math.random() * 3);
+
+function message(r=0) {
+  if (r==0) {
+    return right[Math.floor(Math.random() * right.length)]
+  } else
+  { 
+    return wrong[Math.floor(Math.random() * wrong.length)]
+    }
+}
 
 $("#exercise").text(`${system[aufgabe]}${all[aufgabe]}`);
 
@@ -32,39 +42,39 @@ $("#systems-form").submit((e) => {
   const d = $("#dez").val();
   const h = $("#hex").val().toUpperCase();
   const b = $("#bin").val();
-  const errMessage  = wrong[Math.floor(Math.random() * wrong.length)];
-  const succMessage = right[Math.floor(Math.random() * right.length)];
-  // const errMessage = "Hier stimmt was nicht!";
+
   if (d == all[0] && h== all[1] && b == all[2]) {
     $(".error").hide();
     $("#richtig").slideDown();
-    $("#dezErr").show().text(right[Math.floor(Math.random() * right.length)]);
-    $("#hexErr").show().text(right[Math.floor(Math.random() * right.length)]);
-    $("#binErr").show().text(right[Math.floor(Math.random() * right.length)]);
+    $(".success.richtig").text(richtig[Math.floor(Math.random() * richtig.length)]);
+    $("#dezErr").show().text(message);
+    $("#hexErr").show().text(message);
+    $("#binErr").show().text(message);
     $("#submit").hide();
     $("#falsch").hide();
   }
   else {
     $(".error").hide();
     if ( d != all[0] ) {
-      $("#dezErr").show().text(wrong[Math.floor(Math.random() * wrong.length)]);
+      $("#dezErr").show().text(message(1));
     }
     else {
-      $("#dezErr").show().text(right[Math.floor(Math.random() * right.length)]);
+      $("#dezErr").show().text(message);
     }
     if (h != all[1]) {
-      $("#hexErr").show().text(wrong[Math.floor(Math.random() * wrong.length)]);
+      $("#hexErr").show().text(message(1));
     }
     else {
-      $("#hexErr").show().text(right[Math.floor(Math.random() * right.length)]);
+      $("#hexErr").show().text(message);
     }
     if (b != all[2]) {
-      $("#binErr").show().text(wrong[Math.floor(Math.random() * wrong.length)]);
+      $("#binErr").show().text(message(1));
     }
     else {
-      $("#binErr").show().text(right[Math.floor(Math.random() * right.length)]);
+      $("#binErr").show().text(message);
     }
     $("#falsch").slideDown();
+    $(".success.falsch").text(falsch[Math.floor(Math.random() * falsch.length)]);
     // $(".form-container").slideUp();
   }
 });
